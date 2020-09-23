eeef4120632b6271d73d1240807e3e6c7c5cd3da
Dmitry Shamin <dmitry.shamin@gmail.com>
2020-09-23 15:40:00 +0300

    Add russian language file

---
+++ lang/ru/lang.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'User Plus',
+        'description' => 'Добавляет несколько новых полей для плагина RainLab User.',
+        'author' => 'Gergő Szabó'
+    ],
+    'personal' => [
+        'tab' => 'Личные данные',
+        'gender' => 'Пол',
+        'telephone' => 'Телефон',
+        'job' => 'Профессия',
+        'company' => 'Компания',
+        'about' => 'О пользователе'
+    ],
+    'internet' => [
+        'tab' => 'Интернет',
+        'webpage' => 'Сайт',
+        'blog' => 'Блог',
+        'facebook' => 'Facebook',
+        'twitter' => 'Twitter',
+        'skype' => 'Skype',
+        'icq' => 'ICQ'
+    ],
+    'gender' => [
+        'unknown' => 'не указан',
+        'female' => 'женщина',
+        'male' => 'мужчина'
+    ],
+    'comment' => 'Комментарий',
+    'widget' => [
+        'title' => 'Статистика пользователей',
+        'show_total' => 'Показать всего',
+        'show_active' => 'Показать активных',
+        'show_inactive' => 'Показать не активных',
+        'show_deleted' => 'Показать удалённых',
+        'show_guest' => 'Показать гостей',
+        'show_superuser' => 'Показать админов',
+        'total' => 'Всего',
+        'active' => 'Активных',
+        'inactive' => 'Не активных',
+        'deleted' => 'Удалённых',
+        'guest' => 'Гостей',
+        'superuser' => 'Админов',
+        'permission' => 'Управлять виджетом на Дашборде'
+    ]
+];
